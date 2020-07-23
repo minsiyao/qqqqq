2212aab78fb8fbf0667f99dac663354c6d83a2b3
MSY <2419778065@qq.com>
2020-07-23 14:35:04 +0800

    v1.0

---
+++ src/main/java/com/msy/qqqqq/QqqqqApplication.java
@@ -0,0 +1,13 @@
+package com.msy.qqqqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqqqqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QqqqqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msy/qqqqq/QqqqqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msy.qqqqq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqqqqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
